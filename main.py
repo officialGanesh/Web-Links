a0e543743b642ef4d5c2a7209762ddcfbc533aea
Ganesh <ganuofficial2795@gmail.com>
2021-04-30 07:57:29 +0530

    Links found

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-# import required modules
 
 import requests ,lxml
 from bs4 import BeautifulSoup as bs
 
+def Find_links():
 
+    try:
+        
+        r = requests.get('https://in.linkedin.com/')
+        r.raise_for_status
 
+        soup = bs(r.text,'lxml')
+        print(soup.title.text)
 
+        links = set((soup.find_all('a')))
+
+        for link in links:
+            print(link.get('href'))
+
+    except Exception as e:
+        print("Something Went Wrong 💢")
 
 
 if __name__ == "__main__":
 
+    Find_links()
     print("Code Completed 🔥")
